961ec6d4116e8f703f66848951041607d5c6b8c9
Jairo Leite <jairo.leite@novotempo.com>
2018-10-15 10:34:32 -0300

    criação de sistema financeiro

---
+++ FinancialMe/src/main/java/com/me/financial/FinancialMeApplication.java
@@ -0,0 +1,12 @@
+package com.me.financial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialMeApplication.class, args);
+	}
+}
